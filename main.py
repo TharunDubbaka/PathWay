3e46edd48eba22b222f0ae7487ff883cc1938021
TharunDubbaka <rajithadubbaka4005@gmail.com>
2026-07-31 10:13:31 +0530

    connected front and back

--- main.py
+++ main.py
@@ -41,7 +41,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://path-way-theta.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
